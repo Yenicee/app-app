b0d819acc89f7c1e5d80c155acb844f85cdf578e
Yenicee Vazquez <perezyenice@gmail.com>
2022-10-26 22:32:39 -0300

    agregue cosas

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",{"nombre":"Yenice"})
+
+def index_uno(request):
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5,6,7,8,9]} 
+    )
+
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, index_uno
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
+    path("mostrar-notas/", index_uno),
 ]
